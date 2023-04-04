e5aae0b1e2ee0dca5eae8e856927a2b46e9f98fd
rafaelanjosjesus <veigarnodraiven@gmail.com>
2023-04-04 15:29:16 -0300

    I finish my multiplication Table

---
+++ scripts.js
@@ -0,0 +1,43 @@
+let one = 1;
+for (let contador = 1; contador <= 10; contador += 1) {
+    console.log(one * contador)
+}
+let two = 2;
+for (let contador = 1; contador <= 10; contador += 1) {
+    console.log(two * contador)
+}
+let tree = 3;
+for (let contador = 1; contador <= 10; contador += 1) {
+    console.log(tree * contador)
+}
+let foor = 4;
+for (let contador = 1; contador <= 10; contador += 1) {
+    console.log(foor * contador)
+}
+
+let five = 5;
+for (let contador = 1; contador <= 10; contador += 1) {
+    console.log(five * contador)
+}
+let six = 6;
+for (let contador = 1; contador <= 10; contador += 1) {
+    console.log(six * contador)
+}
+let seven = 7;
+for (let contador = 1; contador <= 10; contador += 1) {
+    console.log(seven * contador)
+}
+let eight = 8;
+for (let contador = 1; contador <= 10; contador += 1) {
+    console.log(eight * contador)
+}
+let nine = 9;
+for (let contador = 1; contador <= 10; contador += 1) {
+    console.log(nine * contador)
+}
+let ten = 10;
+for (let contador = 1; contador <= 10; contador += 1) {
+    console.log(ten * contador)
+}
+
+
